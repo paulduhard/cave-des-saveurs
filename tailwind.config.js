ae3ea2b03e24785ecbb7b7a850b7fa5176b2e724
Paul <paul@duhard.com>
2024-03-08 14:34:36 +0100

    add font-family, colors and favicon.svg

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sofia': ['sofia-pro', 'sans-serif'],
+        'span': ['span', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
