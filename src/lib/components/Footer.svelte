6b7dd989d2ded39fd43df01669bfbc8ededc2dc0
Paul <paul@duhard.com>
2024-09-09 18:50:02 +0200

    fix vercel error deploy

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { PrismicLink } from '@prismicio/svelte';
+
+	/** @type {import("@prismicio/client").Content.SettingsDocument} */
+	export let settings;
+</script>
+
+<footer>
+	<nav class="bg-primary">
+		<span class="sr-only">{settings.data.site_title} page d'accueil</span>
+
+		<ul>
+			{#each settings.data.navigation as item (item.label)}
+				<li>
+					<PrismicLink field={item.link}>
+						{item.label}
+					</PrismicLink>
+				</li>
+			{/each}
+		</ul>
+	</nav>
+</footer>
---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { PrismicLink } from '@prismicio/svelte';
+
+	/** @type {import("@prismicio/client").Content.SettingsDocument} */
+	export let settings;
+</script>
+
+<header>Test</header>
