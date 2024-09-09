ff3919c9fb5537922f0c2d178a4f35905a6e7c54
Paul <paul@duhard.com>
2024-09-09 22:45:53 +0200

    update header and footer

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,8 +1,23 @@
 <script>
-	import { PrismicLink } from '@prismicio/svelte';
+	import { PrismicImage, PrismicLink } from '@prismicio/svelte';
 
 	/** @type {import("@prismicio/client").Content.SettingsDocument} */
 	export let settings;
 </script>
 
-<header>Test</header>
+<header class="flex min-h-20 items-center bg-secondary">
+	<!-- <PrismicImage field={data.logo_header} /> -->
+	<nav>
+		<span class="sr-only">{settings.data.site_title} page d'accueil</span>
+
+		<ul class="flex content-center gap-16">
+			{#each settings.data.navigation as item (item.label)}
+				<li class="uppercase">
+					<PrismicLink field={item.link} class="text-white">
+						{item.label}
+					</PrismicLink>
+				</li>
+			{/each}
+		</ul>
+	</nav>
+</header>
