237e096db9afe75fd3b72867307d6e873b2b617e
Paul <paul@duhard.com>
2024-09-28 11:49:42 +0200

    add Socials Components

---
+++ src/lib/components/Socials.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { PrismicRichText, PrismicImage, PrismicLink } from '@prismicio/svelte';
+
+	/** @type {import("@prismicio/client").Content.SettingsDocument} */
+	export let settings;
+</script>
+
+<section class="flex flex-col justify-center py-24 text-center md:h-[395px]">
+	<PrismicRichText field={settings.data.texte} class="px-24 md:px-0 md:text-4xl" />
+	<div class="flex items-baseline justify-center gap-12 pt-8">
+		{#each settings.data.item as item}
+			<PrismicLink field={item.lien}>
+				<PrismicImage field={item.icone} />
+			</PrismicLink>
+		{/each}
+	</div>
+</section>
