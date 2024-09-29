dac1a34383dc23538c2cf74483a962cc0bbed374
Paul <paul@duhard.com>
2024-09-29 22:14:55 +0200

    styling appellation filter

--- src/lib/components/Aside.svelte
+++ src/lib/components/Aside.svelte
@@ -9,8 +9,6 @@
 	};
 	export let handleFilterChange;
 	export let appellationNames = {};
-
-	$: console.log('Displayed appellations in Aside:', filterData.displayedAppellations);
 </script>
 
 <aside class="bg-gray-100 w-1/5 p-4">
@@ -18,41 +16,40 @@
 		<h3 class="mb-2 text-xl uppercase">Nos vins par domaines</h3>
 		{#if filterData.domains && filterData.domains.length > 0}
 			{#each filterData.domains as domain}
-				<label class="mb-2 block cursor-pointer">
-					<input
-						type="radio"
-						name="domain"
-						value={domain.uid}
-						checked={filterData.selectedDomain === domain.uid}
-						on:change={() => handleFilterChange('domain', domain.uid)}
-						class="hidden"
-					/>
-					<span
-						class="{filterData.selectedDomain === domain.uid
-							? 'font-bold underline'
-							: ''} hover:text-gray-700"
-					>
-						{domain.name}
-					</span>
-				</label>
+				<div class="mb-2">
+					<label class="block cursor-pointer">
+						<input
+							type="radio"
+							name="domain"
+							value={domain.uid}
+							checked={filterData.selectedDomain === domain.uid}
+							on:change={() => handleFilterChange('domain', domain.uid)}
+							class="hidden"
+						/>
+						<span
+							class="{filterData.selectedDomain === domain.uid
+								? 'font-bold underline'
+								: ''} hover:text-gray-700"
+						>
+							{domain.name}
+						</span>
+					</label>
+
+					{#if filterData.selectedDomain === domain.uid && filterData.displayedAppellations.length > 0}
+						<ul class="ml-4 mt-2 pl-2 text-sm">
+							{#each filterData.displayedAppellations as appellation}
+								<li>{appellationNames[appellation.uid] || 'Nom inconnu'}</li>
+							{/each}
+						</ul>
+					{:else if filterData.selectedDomain === domain.uid}
+						<p class="ml-4 mt-2 text-sm italic">Aucune appellation trouvée</p>
+					{/if}
+				</div>
 			{/each}
 		{:else}
 			<p>Aucun domaine disponible</p>
 		{/if}
 	</div>
-
-	{#if filterData.selectedDomain && filterData.displayedAppellations.length > 0}
-		<div class="mb-6">
-			<h4 class="mb-2 text-lg">Appellations:</h4>
-			<ul class="list-disc pl-5">
-				{#each filterData.displayedAppellations as appellation}
-					<li>{appellationNames[appellation.uid] || 'Nom inconnu'}</li>
-				{/each}
-			</ul>
-		</div>
-	{:else if filterData.selectedDomain}
-		<p>Aucune appellation trouvée pour ce domaine.</p>
-	{/if}
 
 	<div class="mb-6">
 		<h3 class="mb-2 text-xl uppercase">Couleurs</h3>
